c82f2a8f1f207f9bf16990a1eb31ace776e3f21a
Teri <kenini@gmail.com>
2019-08-10 09:14:25 -0400

    Update JSproject.js

--- JSproject.js
+++ JSproject.js
@@ -1,3 +1,12 @@
+// For this project you will be building a generic website that will seem to ask mundane questions to normal users, but you secretly are looking for a spy! Anyone visiting your website will be asked a series of questions, only the Spy will be able to give specific answers you expect. If all questions are answered correctly, you will post a secret message in console for the Spy to read!
+// 
+// Here are the four conditions you will check for the spy:
+// 
+// The Spy has the same first letter of her First Name and Last Name
+// The Spy is between the Age of 20 and 30 (exclusive of 20 and 30)
+// The Spy is at least 170 centimeters tall.
+// The Spy has a pet name that ends with the letter "y".
+
 var name = prompt("What is your first name?");
 var surname = prompt("what is your last name?");
 var age = prompt("what is your age?");
